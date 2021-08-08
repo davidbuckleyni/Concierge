a826ead15eb2734608230bb1e309d19903069e13
davidbuckleyni <davidbuckleyweb@gmail.com>
2021-08-08 14:11:24 +0100

    Full commit of changes

---
+++ Concierge.Models/User.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Concierge.Models
+{
+    
+     public class User
+    {
+
+        public enum UserTypeEnum
+        {
+            Customer = 1,
+            Agent = 2,
+            Manager = 3,
+            Accounts = 4,
+            Admin = 5
+
+        }
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+
+        public int? UserType { get; set; }
+        public string? Username { get; set; }
+        public string? Email { get; set; }
+
+        [JsonIgnore]
+        public string? Password { get; set; }
+
+        public string? ConfirmPassword { get; set; }
+
+        [JsonIgnore]
+        public List<RefreshToken> RefreshTokens { get; set; }
+ 
+    }
+}
---
+++ Concierge.Models/User/AuthenicationResponseOjbect.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Concierge.Models
+{
+    public partial class AuthenicationResponseOjbect
+    {
+        public long Id { get; set; }
+
+
+        public Guid? StoreId { get; set; }
+
+        public Guid? UserId { get; set; }
+        public object FirstName { get; set; }
+
+        public object LastName { get; set; }
+
+        public string Username { get; set; }
+
+        public string JwtToken { get; set; }
+
+        public object RefreshToken { get; set; }
+
+
+    }
+
+}
